95c2ca556103df454b8df184199a9f42f223702f
Challa-Aditya <caditya02@gmail.com>
2024-11-03 19:11:50 +0530

    hamburger icon added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import { RxHamburgerMenu } from "react-icons/rx";
 
 
 
@@ -26,7 +27,7 @@ const Navbar = () => {
           <div className="w-full h-16 fixed top-0 shadow-sm shadow-[#2A0E50]/50 bg-[#171515a0] backdrop-blur-sm z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
             <Link href="/" className='text-3xl ml-4 pl-4 highlight-text challa'>Challa</Link>
-                <button onClick={toggleMenu} className={`Hamburger`}>click</button>
+                <button onClick={toggleMenu} className={`Hamburger`}><RxHamburgerMenu/></button>
             <ul className={`Navlinks ${isOpen ? "showNavbar":""}`} onClick={toggleMenu} >
               <li className='text-2xl hover:text-3xl highlight-text'>
                 <div className='px-3'>
